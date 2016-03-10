63011e34c36b8a0611d401de2028a4c22be9b9df
Eric Miller <epmiller8464@gmail.com>
2016-03-10 16:37:11 -0600

    initial commit

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-var opts = {
-    transports: [
-        'websocket',
-        'polling'
-        //'xhr-polling',
-        //'jsonp-polling'
-    ],
-    log: true,
-    origins: '*:*'
-};
 //var sio = require('socket.io')
 //var io = require('socket.io')(http,opts);
 //var io = sio(http, opts)
@@ -46,87 +36,3 @@ require('./IoServer')(http)
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
-
-
-////var express = require('express');
-////var io = require('socket.io')
-//var bodyParser = require('body-parser');
-//var minimist = require('minimist');
-//var uuid = require('node-uuid');
-////var ws = require('ws');
-//var morgan = require('morgan')
-//var argv = minimist(process.argv.slice(2), {
-//    default: {
-//        as_uri: 'http://localhost:8080',
-//        ws_uri: 'ws://192.168.99.100:8888/kurento'
-//    }
-//});
-//
-//var asUrl = url.parse(argv.as_uri);
-//var port = asUrl.port;
-//var app = express();
-//app.use(express.static(path.join(__dirname, 'static')));
-
-//var app = express().createServer();
-//
-//app.use(bodyParser.json());
-//app.use(express.static(path.join(__dirname, 'static')));
-//app.use(morgan('dev'))
-//
-//var server = app.listen(port, function () {
-//    console.log('node-room-server started');
-//    console.log('Open ' + url.format(asUrl) + ' with a WebRTC capable browser');
-//});
-////var server = require('http').Server(app);
-//
-//app.get('/', function (req, res) {
-//    res.sendfile(__dirname + '/api.html');
-//});
-//
-//
-//io.on('connection', function (socket) {
-//    socket.emit('news', {hello: 'world'});
-//    socket.on('my other event', function (data) {
-//        console.log(data);
-//    });
-//});
-//
-//var io = require('socket.io')(app);
-//
-//io.on('connection', function (socket) {
-//    socket.emit('news', { hello: 'world' });
-//    socket.on('my other event', function (data) {
-//        console.log(data);
-//    });
-//});
-
-//var express = require('express')
-//var app = express();
-//var server = require('http').createServer(app);
-//var io = require('socket.io')(server,opts);
-//
-//app.use(express.static(path.join(__dirname, 'static')));
-//app.get('/', function (req, res) {
-//    res.sendfile(__dirname + '/static/api.html');
-//});
-//server.listen(port, function () {
-//    console.log('Server listening at port %d', port);
-//});
-
-//var sso = require('socket.io')
-//var server = require('http').Server(app);
-//
-//var io = new sso(server, opts);
-//
-//server.listen(8080);
-//
-//app.get('/', function (req, res) {
-//    res.sendfile(__dirname + '/static/api.html');
-//});
-//
-//io.sockets.on('connection', function (socket) {
-//    socket.emit('news', {hello: 'world'});
-//    socket.on('my other event', function (data) {
-//console.log(socket);
-//});
-//});
